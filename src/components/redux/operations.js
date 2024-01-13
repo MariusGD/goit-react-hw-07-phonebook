87d3b953617ed83759f30ac006434f72e338d4da
MariusGD <gagumarius2015@gmail.com>
2024-01-13 21:33:04 +0200

    update 1

---
+++ src/components/redux/operations.js
@@ -0,0 +1,40 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://659d18c4633f9aee7908990d.mockapi.io/';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async function (_, { rejectWithValue }) {
+    try {
+      const { data } = await axios.get('/contacts');
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async function (contact, { rejectWithValue }) {
+    try {
+      const { data } = await axios.post('/contacts', contact);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async function (contactId, { rejectWithValue }) {
+    try {
+      const { data } = await axios.delete(`/contacts/${contactId}`);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
